9cd8ddc382f0824a87a2b0081d47003e3a3c4bd6
cooper <cooper6334@gmail.com>
2012-09-17 18:28:52 +0800

    Debug 7 people

--- src/ntu/real/sense/Global.java
+++ src/ntu/real/sense/Global.java
@@ -19,9 +19,9 @@ public class Global {
 	static int selectWay;
 	static String name;
 	static final String[] userName = { "Allen", "Bob", "Cooper", "David",
-		"Eric", "Frank" };
+			"Eric", "Frank", "Grace", "Herry" };
 	static final int[] userColor = { 0xffdda628, 0xff9bba3b, 0xff5bb6b9,
-		0xffc9544b, 0xffc5544c, Color.DKGRAY };
+			0xffc9544b, 0xffc5544c, Color.BLUE, Color.MAGENTA, Color.DKGRAY };
 
 	// static String mMAC = "";
 	// final static UUID mUUID = UUID
